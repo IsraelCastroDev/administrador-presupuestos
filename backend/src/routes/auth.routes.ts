95d62ab9d3087cc5793bd0ef3e8d6a0231309ada
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-29 16:24:42 -0500

    creando middleware para validar usuarios

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from "express";
 import { AuthController } from "../controllers";
 import {
+  handleValidateAccountExists,
   handleValidateUserExists,
+  handleValidateUserId,
   handleValidateUserInput,
 } from "../middlewares/user-middleware";
 import { handleInputErrors } from "../middlewares/validation";
 
 const authRoutes = Router();
 
+authRoutes.param("userId", handleValidateUserId);
+
 authRoutes.post(
   "/create-account",
   handleValidateUserInput,
   handleInputErrors,
-  handleValidateUserExists,
+  handleValidateAccountExists,
   AuthController.create
 );
 
