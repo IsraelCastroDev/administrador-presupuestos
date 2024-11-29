27d5fcb679b7c7a16e3a759485d23d3a934aefb5
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-29 12:29:51 -0500

    agregando expense a request express

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import Budget from "../../models/Budget";
+import Expense from "../../models/Expense";
 
 declare global {
   namespace Express {
     interface Request {
       budget?: Budget;
+      expense?: Expense;
     }
   }
 }
