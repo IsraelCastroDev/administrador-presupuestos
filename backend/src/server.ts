c29fa1f83e4766281d86f9de4fab13564da88830
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-28 22:03:13 -0500

    creando el enrutador para los endpoints de budget

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,12 @@
 import express from "express";
 import morgan from "morgan";
-import { db } from "./config/db";
-
-export async function connectDB() {
-  try {
-    await db.authenticate();
-    db.sync();
-    console.log("Conexión exitosa a la Base de Datos");
-  } catch (error) {
-    console.log(error);
-  }
-}
+import budgetRoutes from "./routes/budget.routes";
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
+
+server.use("/api/budgets", budgetRoutes);
 
 export default server;
