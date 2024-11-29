e78a9ce805ec4d3f3aba617a8a871115182b29f4
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-29 13:06:37 -0500

    obteniendo los gastos de un presupuesto

--- backend/src/controllers/BudgetController.ts
+++ backend/src/controllers/BudgetController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { Budget } from "../models";
+import Budget from "../models/Budget";
+import Expense from "../models/Expense";
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
@@ -34,7 +35,9 @@ export class BudgetController {
   };
 
   static getById = async (req: Request, res: Response) => {
-    const { budget } = req;
+    const budget = await Budget.findByPk(req.budget!.id, {
+      include: [Expense],
+    });
 
     res.status(200).json(budget);
   };
