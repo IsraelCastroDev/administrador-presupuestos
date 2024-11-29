671dfb68e3f1e9bc7bb193f6d42a696e65f54339
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-28 22:02:27 -0500

    funcion para conectar a la bd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,16 @@
 import "dotenv/config";
-import server, { connectDB } from "./server";
+import server from "./server";
+import { db } from "./config/db";
+
+export async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión exitosa a la Base de Datos");
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 connectDB();
 
