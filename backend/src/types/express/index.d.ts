07ceee4053974e4df29efafb7b7c54cf2cc96ab8
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-12-02 16:11:46 -0500

    extendiendo con user el Request de Express

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 import Budget from "../../models/Budget";
 import Expense from "../../models/Expense";
+import User from "../../models/User";
 
 declare global {
   namespace Express {
     interface Request {
       budget?: Budget;
       expense?: Expense;
+      user?: User;
     }
   }
 }
