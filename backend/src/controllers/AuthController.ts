d628886a5a2ebeddd0dc3917433ac2df9c1648c1
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-12-02 15:39:20 -0500

    controlador para la logica de login

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -3,6 +3,8 @@ import User from "../models/User";
 import { hashPassword } from "../utils";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../service/AuthEmail";
+import { checkPassword } from "../utils/auth";
+import { generateJWT } from "../utils/jwt";
 
 class AuthController {
   static getAll = async (req: Request, res: Response) => {
@@ -52,6 +54,41 @@ class AuthController {
       await user.save();
 
       res.status(200).json({ message: "Cuenta confirmada con éxito" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error al crear la cuenta" });
+    }
+  };
+
+  static login = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        res.status(404).json({ error: "El usuario no existe" });
+        return;
+      }
+
+      // comprobar si ha confirmado su cuenta
+      if (!user.confirmed) {
+        res.status(403).json({ error: "La cuenta no ha sido confirmada" });
+        return;
+      }
+
+      // comprobar contraseña
+      const isCorrectPassword = await checkPassword(password, user.password);
+
+      if (!isCorrectPassword) {
+        res.status(401).json({ error: "Contraseña incorrecta" });
+        return;
+      }
+
+      // crear token
+      const jwtToken = generateJWT({ id: user.id });
+
+      res.send(jwtToken);
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: "Ocurrió un error al crear la cuenta" });
