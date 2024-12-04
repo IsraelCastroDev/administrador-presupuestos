ba4c66cf093e874f4a552bd0e592c4d4da52c7f3
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-12-04 11:49:25 -0500

    pasando codigo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,5 @@
 import "dotenv/config";
 import server from "./server";
-import { db } from "./config/db";
-
-export async function connectDB() {
-  try {
-    await db.authenticate();
-    db.sync();
-    console.log("Conexión exitosa a la Base de Datos");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-connectDB();
 
 const PORT = process.env.PORT ?? 4000;
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,20 @@ import express from "express";
 import morgan from "morgan";
 import { budgetRoutes, authRoutes } from "./routes";
 
+import { db } from "./config/db";
+
+export async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión exitosa a la Base de Datos");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectDB();
+
 const server = express();
 
 server.use(morgan("dev"));
@@ -9,5 +23,9 @@ server.use(express.json());
 
 server.use("/api/budgets", budgetRoutes);
 server.use("/api/auth", authRoutes);
+//test
+server.use("/", (req, res) => {
+  res.send("toko ok");
+});
 
 export default server;
